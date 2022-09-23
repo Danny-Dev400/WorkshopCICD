8767281c2293ab4322751a540998b7aad56f8f2d
Danny-Dev <df.salazar0402.gmail.com>
2022-09-22 22:44:45 -0500

    Add Chrome and edge options to the driver

--- src/test/java/UI/Drivers/ChromeDriver/ChromeDriverS.java
+++ src/test/java/UI/Drivers/ChromeDriver/ChromeDriverS.java
@@ -3,11 +3,16 @@ package UI.Drivers.ChromeDriver;
 import UI.Drivers.IDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class ChromeDriverS implements IDriver {
     @Override
     public WebDriver loadDriver() {
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\src\\test\\java\\UI\\Drivers\\ChromeDriver\\chromedriver.exe");
-        return new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        options.addArguments("--disable-dev-shm-usage");
+        return new ChromeDriver(options);
     }
 }
--- src/test/java/UI/Drivers/EdgeDriver/EdgeDriverS.java
+++ src/test/java/UI/Drivers/EdgeDriver/EdgeDriverS.java
@@ -2,11 +2,16 @@ package UI.Drivers.EdgeDriver;
 
 import UI.Drivers.IDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 public class EdgeDriverS implements IDriver {
     @Override
     public EdgeDriver loadDriver() {
         System.setProperty("webdriver.edge.driver", System.getProperty("user.dir") + "\\src\\test\\java\\UI\\Drivers\\EdgeDriver\\msedgedriver.exe");
-        return new EdgeDriver();
+        EdgeOptions options = new EdgeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        options.addArguments("--disable-dev-shm-usage");
+        return new EdgeDriver(options);
     }
 }
